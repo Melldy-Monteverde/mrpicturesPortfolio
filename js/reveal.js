e89f49c0b750e2f369f6a19d10fa326979a66341
Lexther Monteverde <monteverdelex@gmail.com>
2023-05-27 12:03:20 -0300

    updates, add scrollreveals

---
+++ js/reveal.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal()
+
+sr.reveal('.about-left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '50px'
+})
+
+sr.reveal('.about-right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '50px'
+})
+
+sr.reveal('#portfolio', {
+  duration: 2000
+})
