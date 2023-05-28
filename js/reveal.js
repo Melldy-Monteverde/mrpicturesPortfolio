d3e54ee129deeea4e8097496a2aac8833dd18360
Lexther Monteverde <monteverdelex@gmail.com>
2023-05-28 17:53:58 -0300

    updates, remove form and add links for wpp and mial

--- js/reveal.js
+++ js/reveal.js
@@ -3,13 +3,13 @@ window.sr = ScrollReveal()
 sr.reveal('.about-left', {
   duration: 2000,
   origin: 'left',
-  distance: '50px'
+  distance: '20px'
 })
 
 sr.reveal('.about-right', {
   duration: 2000,
   origin: 'right',
-  distance: '50px'
+  distance: '20px'
 })
 
 sr.reveal('#portfolio', {
